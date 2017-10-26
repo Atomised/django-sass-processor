726d06e95a3c205689e42bdad4e96a17afe1d5a7
Morgan Faichney <morgan@atomised.coop>
2017-10-26 16:17:24 +0100

    attempt to retrieve settings from db

--- sass_processor/utils.py
+++ sass_processor/utils.py
@@ -18,8 +18,8 @@ def get_db_setting(whl, key):
         whitelabel = Whitelabel.objects.filter(slug=whl).order_by('id')    
         # if whitelabel, load config
         if whitelabel.count() != 0:
-            print getattr(whitelabel[0], key)
-            return getattr(whitelabel[0], key)
+            print getattr(whitelabel[0])
+            return getattr(whitelabel[0])
         
     except AttributeError as e:
         raise TemplateSyntaxError(e.message)
