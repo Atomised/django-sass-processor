a6efc6280a692ea095a2f7dc24ce63bd273c2803
Morgan Faichney <morgan@atomised.coop>
2017-10-26 16:30:02 +0100

    attempt to retrieve settings from db

--- sass_processor/utils.py
+++ sass_processor/utils.py
@@ -24,7 +24,7 @@ def get_db_setting(whl):
             #     return getattr(whitelabel[0], key)
             # else:
             print model_to_dict(whitelabel[0], fields=[field.name for field in whitelabel[0]._meta.fields]) 
-            return model_to_dict(whitelabel[0], fields=[field.name for field in whitelabel[0]._meta.fields])whitelabel[0]
+            return model_to_dict(whitelabel[0], fields=[field.name for field in whitelabel[0]._meta.fields])
         
     except AttributeError as e:
         raise TemplateSyntaxError(e.message)
